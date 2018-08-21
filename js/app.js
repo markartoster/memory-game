433a0882d032f80b1809acf494708c036aa15720
Karol Rosiński <markar.toster@gmail.com>
2018-08-21 13:11:00 +0200

    add time flow function

--- js/app.js
+++ js/app.js
@@ -1,8 +1,39 @@
-/*
- * Create a list that holds all of your cards
- */
+const cardList = $('.card');
+const cardListLength = cardList.length;
+
+console.log(cardListLength);
+
+let arrayOfPictures = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube', 'fa fa-leaf',
+'fa fa-bicycle', 'fa fa-bomb','fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', 'fa fa-bolt', 'fa fa-cube',
+'fa fa-leaf', 'fa fa-bicycle', 'fa fa-bomb'];
+
+let psuedoRandomSort = arrayOfPictures.sort(() => 0.5 - Math.random());
 
 
+//Game variables
+let sec = 0;
+let min = 0;
+let timeCounter = 0;
+let counter;
+
+//UI elements
+let gameTimeUI = $(".time");
+
+//Manages timeflow and displays it on ui
+startTimeCounting = () => {
+  counter = setInterval(function() {
+    gameTimeUI.innerHTML = "Time: " + min +" mins "+ sec + " secs";
+    timeCounter = "Time: " + min +" mins "+ sec + " secs";
+    sec++;
+      if(sec === 60) {
+        min++;
+        sec = 0;
+      }
+  },1000);
+
+}
+
+startTimeCounting()
 /*
  * Display the cards on the page
  *   - shuffle the list of cards using the provided "shuffle" method below
