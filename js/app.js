0c003298a91aeb1f83e8a60b154862ab2be4e267
Karol Rosiński <markar.toster@gmail.com>
2018-08-21 20:57:53 +0200

    Add basic game functionallity - card flipping and check condition, also UI update

--- js/app.js
+++ js/app.js
@@ -10,7 +10,7 @@ let arrayOfPictures = ['fa fa-diamond', 'fa fa-paper-plane-o', 'fa fa-anchor', '
 let psuedoRandomSort = arrayOfPictures.sort(() => 0.5 - Math.random());
 
 
-//Game variables
+//Time variables
 let sec = 0;
 let min = 0;
 let timeCounter = 0;
@@ -59,6 +59,79 @@ setPicturesToCards = () => {
     $(".hiddenPicture"+pictureId).removeClass("hiddenPicture"+pictureId);
   }
 }
+
+//Game Variables
+let number = 0;
+let firstCard = "";
+let secondCard = "";
+let numberOfMoves = 0;
+let correctGuess = 0;
+
+//Refresh Cards
+turn = () => {
+  $(firstId).removeClass("open show");
+  $(secondId).removeClass("open show");
+  $(firstId).removeClass("wrong");
+  $(secondId).removeClass("wrong");
+}
+/*
+  On card click
+*/
+$(".card").click(function(event) {
+
+  if(numberOfMoves < 18) {
+    $("#star3").removeClass();
+    $("#star3").addClass("fa fa-star-o");
+  }
+  else if(numberOfMoves < 24) {
+    $("#star2").removeClass();
+    $("#star2").addClass("fa fa-star-o");
+  }
+  else {
+    $("#star1").removeClass();
+    $("#star1").addClass("fa fa-star-o");
+  }
+  if (number < 2) {
+    number++;
+    if(number === 1) {
+      firstCard = event.target.childNodes[1];
+      firstId = document.getElementById(event.target.id);
+      $(firstId).addClass("open show");
+    }
+    else {
+      secondCard = event.target.childNodes[1];
+      secondId = document.getElementById(event.target.id);
+      $(secondId).addClass("open show");
+    }
+
+    if(number === 2) {
+      if(firstCard.isEqualNode(secondCard)) {
+        $(firstId).addClass("match");
+        $(secondId).addClass("match");
+        $(firstId).removeClass("open show");
+        $(secondId).removeClass("open show");
+        number = 0;
+        firstCard = "";
+        secondCard = "";
+        numberOfMoves++;
+        $(".moves").html(numberOfMoves);
+        correctGuess++;
+    }
+      else {
+        if(!(firstCard.isEqualNode(secondCard))) {
+        $(firstId).addClass("wrong");
+        $(secondId).addClass("wrong");
+        setTimeout(turn , 500);
+        number = 0;
+        firstCard = "";
+        secondCard = "";
+        numberOfMoves++;
+        $(".moves").html(numberOfMoves);
+        }
+      }
+    }
+  }
+});
 /*
  * set up the event listener for a card. If a card is clicked:
  *  - display the card's symbol (put this functionality in another function that you call from this one)
